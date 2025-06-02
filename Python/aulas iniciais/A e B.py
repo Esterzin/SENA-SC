b773fda1335daefbf545958dd12e33ad88e7f69b
Esterzin <142187105+Esterzin@users.noreply.github.com>
2025-06-02 13:20:53 -0300

    1° Semestre

---
+++ Python/aulas iniciais/A e B.py
@@ -0,0 +1,7 @@
+le1 = input("digite uma letra: ")
+le2 = input("digite outra letra: ")
+
+if le1 < le2:
+    print(le1, le2)
+else:
+    print(le2, le1)
---
+++ Python/aulas iniciais/aula 2.py
@@ -0,0 +1,18 @@
+n1 = int(input("digite um numero: "))
+n2 = int(input("digite outro numero: "))
+
+soma = n1 + n2
+sub = n1 - n2
+div = n1 / n2
+mult = n1 * n2
+restdiv = n1 % n2
+expo = n1 ** n2
+divint = n1 // n2
+
+print(f"a soma é: ", soma) 
+print(f"a subtração é: ", sub)
+print(f"a divisão é: ", div) 
+print(f"a multiplicação é: ", mult)
+print(f"o resto da divisão é: ", restdiv)
+print(f"a exponenciação é: ", expo)
+print(f"a divisão inteira é: ", divint)
