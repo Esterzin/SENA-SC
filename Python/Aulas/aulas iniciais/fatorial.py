56e1a219fcb92e532db275121a5931e89c7077fe
Esterzin <142187105+Esterzin@users.noreply.github.com>
2025-06-02 15:09:22 -0300

    1° Semestre

---
+++ Python/Aulas/aulas iniciais/fatorial.py
@@ -0,0 +1,7 @@
+num = int(input("digite um numero: "))
+fatorial = 1
+
+for i in range(1, num + 1):
+    fatorial *= i
+
+    print("fatorial = ", fatorial)
---
+++ Python/Aulas/aulas iniciais/retangulo.py
@@ -0,0 +1,6 @@
+base = float(input("digite a base do seu retangulo: "))
+altura = float(input("digite a altura do seu retangulo: "))
+
+area = (base + altura)*2
+
+print(f" a area do seu retangulo é: ", area)
