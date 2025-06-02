bca987dd29cecf218831d25bbbb1541dba1ca4fd
Esterzin <142187105+Esterzin@users.noreply.github.com>
2025-06-02 15:04:19 -0300

    Delete Python/aulas iniciais directory

--- Python/aulas iniciais/media.py
+++
@@ -1,7 +0,0 @@
-nota1 = float(input("digite a média do aluno 1: "))
-nota2 = float(input("digite a média do aluno 2: "))
-nota3 = float(input("digite a média do aluno 3: "))
-
-media = (nota1 + nota2 + nota3)/3
-
-print(" A media  do aluno é ", media)
--- Python/aulas iniciais/negativo ou positivo.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("digite um numero: "))
-if numero > 0:
-    print("seu numero é positivo")
-elif numero < 0:
-     print("seu numero é negativo")
-else:
-     print("seu numero é zero")
-   
