14adeae307d8e5babc3de9c50c451f7378bddef4
Esterzin <142187105+Esterzin@users.noreply.github.com>
2025-06-02 15:04:57 -0300

    1° Semestre

---
+++ Python/aulas iniciais/negativo ou positivo.py
@@ -0,0 +1,8 @@
+numero = int(input("digite um numero: "))
+if numero > 0:
+    print("seu numero é positivo")
+elif numero < 0:
+     print("seu numero é negativo")
+else:
+     print("seu numero é zero")
+   
